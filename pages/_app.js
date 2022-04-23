e155c7e787c746d3a2306dbdcfe791ba9bc86c09
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-04-23 14:38:08 +0300

    Started Firebase POC.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
 import '../styles/globals.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-function MyApp({ Component, pageProps }) {
+const firebaseConfig = {
+  apiKey: "AIzaSyBnSrI75QWNR8y4lKmUKYPDjcwrf11fqNI",
+  authDomain: "alex-code4ro.firebaseapp.com",
+  projectId: "alex-code4ro",
+  storageBucket: "alex-code4ro.appspot.com",
+  messagingSenderId: "878504888355",
+  appId: "1:878504888355:web:d1f3d02c4489563a834996",
+  measurementId: "G-5DFT7CENX9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+isSupported().then(() => {
+  const analytics = getAnalytics(app);
+});
+
+function AlexApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
-
   return getLayout(<Component {...pageProps} />)
 }
 
-export default MyApp
+export default AlexApp
