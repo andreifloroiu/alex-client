bca6cb7f90510c6ab7267f5de4e4636e00275c22
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-04-27 11:45:37 +0000

    Refactored.

--- src/alex-client/pages/init.tsx
+++ src/alex-client/pages/init.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import AlexFooter from '../components/AlexFooter'
 import styles from '../styles/Main.module.css'
 
 const InitProfile: NextPage = () => {
@@ -19,18 +19,7 @@ const InitProfile: NextPage = () => {
   
         </main>
   
-        <footer className={styles.footer}>
-          <a
-            href="https://code4.ro"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image src="/favicon.ico" alt="Code4Romania Logo" width={16} height={16} />
-            </span>
-          </a>
-        </footer>
+        <AlexFooter />
       </div>
     )
   }
