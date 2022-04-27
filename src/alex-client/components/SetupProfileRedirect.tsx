57387cda4670e5c9133d0d565ecfd31a42089107
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-04-27 19:56:03 +0000

    Updated setup profile flow and enter page.

--- src/alex-client/components/SetupProfileRedirect.tsx
+++ src/alex-client/components/SetupProfileRedirect.tsx
@@ -4,8 +4,13 @@ import { getIsCSOProfileCreated } from "../lib/firebase";
 const SetupProfileRedirect = () => {
     const router = useRouter()
     getIsCSOProfileCreated()
-        .then((data) => { if (!data) return router.push('init/'); })
-        .catch((e) => { console.error(e) })
+        .then((data) => { if (!data) return router.push('init/') })
+        .catch((e) => { 
+            console.error(JSON.stringify(e, null, 2))
+            if (e.code == 'permission-denied') {
+                return router.push('enter/')
+            }
+        })
     return (null)
 }
 
--- src/alex-client/pages/enter.tsx
+++ src/alex-client/pages/enter.tsx
@@ -1,27 +1,47 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useContext } from 'react'
 import AlexFooter from '../components/AlexFooter'
+import { UserContext } from '../lib/context'
+import { auth } from '../lib/firebase'
 import styles from '../styles/Main.module.css'
 
+
+
 const Enter: NextPage = () => {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>ALEX</title>
-          <meta name="description" content="Enter ALEX - RO NGO APP" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-  
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Enter ALEX!
-          </h1>
-  
-        </main>
-  
-        <AlexFooter />
-      </div>
-    )
-  }
-  
-  export default Enter
+  const { user } = useContext(UserContext)
+  const isSignedIn = !!user
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login - ALEX</title>
+        <meta name="description" content="Enter ALEX" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {isSignedIn ?  <SignOutLayout /> : <SignInLayout />}
+      <AlexFooter />
+    </div>
+  )
+}
+export default Enter
+// Sign out button
+function SignOutLayout() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Exit ALEX!
+      </h1>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    </main>
+  )
+}
+
+function SignInLayout() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Enter ALEX!
+      </h1>
+    </main>
+  )
+}
