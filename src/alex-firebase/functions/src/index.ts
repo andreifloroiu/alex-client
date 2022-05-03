a220c3e9cd374dca5d652792b9dd1ddd28cb67c1
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-05-03 19:01:12 +0000

    Implemented first function.

--- src/alex-firebase/functions/src/index.ts
+++ src/alex-firebase/functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const checkAppConfigured = functions.https.onRequest(async (request, response) => {
+  const doc = await admin.app().firestore().doc("/CSOProfile/default").get();
+  const data = doc.data();
+  const isProfileComplete = data?.isProfileComplete ?? false;
+  const hasFinancialData = data?.hasFinancialData ?? false;
+  const hasBankAccount = data?.hasBankAccount ?? false;
+  response.json({isProfileComplete, hasFinancialData, hasBankAccount});
+});
