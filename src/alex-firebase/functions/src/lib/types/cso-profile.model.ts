5d3a4b87a6e53cdb508934d687754c6165ae2a32
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-05-05 09:10:19 +0000

    Added CSO profile data contract.

---
+++ src/alex-firebase/functions/src/lib/types/cso-profile.model.ts
@@ -0,0 +1,21 @@
+export interface CSOProfile {
+    fiscalId: string
+    name: string
+    registerName: string
+    legalRepresentative: CSOLegalRepresentative
+    configureState: CSOCompletionPhase
+}
+export interface CSOLegalRepresentative {
+    firstname: string
+    surname: string
+    email: string
+    phone: string
+    address: string
+}
+export interface CSOCompletionPhase {
+    hasLegalInfo: false,
+    hasLegalRepresentative: false,
+    hasFinancialData: false,
+    hasBankAccounts: false,
+    isComplete: false
+}
